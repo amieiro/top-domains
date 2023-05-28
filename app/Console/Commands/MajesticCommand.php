9c28867ac795102353727e2f24df6f534f7ae4ed
Jesús Amieiro <1667814+amieiro@users.noreply.github.com>
2023-05-28 12:28:24 +0200

    Add concurrent HTTP queries and update the readme

--- app/Console/Commands/MajesticCommand.php
+++ app/Console/Commands/MajesticCommand.php
@@ -2,11 +2,11 @@
 
 namespace App\Console\Commands;
 
-use GuzzleHttp\Promise\Promise;
-use GuzzleHttp\Promise\PromiseInterface;
+use Carbon\Carbon;
+use GuzzleHttp\Client;
+use GuzzleHttp\Pool;
 use GuzzleHttp\Psr7\Response;
 use Illuminate\Console\Command;
-use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Storage;
 
 class MajesticCommand extends Command {
@@ -24,11 +24,82 @@ class MajesticCommand extends Command {
 	 */
 	protected $description = 'Scrap the Majestic top 1 million websites and shows the number of websites using WordPress.';
 
-	protected $URL = 'https://downloads.majestic.com/majestic_million.csv';
-	protected $CSV_file = 'majestic_million.csv';
-	protected $number_of_websites_tested = 0;
-	protected $number_of_websites_using_wordpress = 0;
-	protected $userAgent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36';
+	/**
+	 * The URL of the CSV file containing the top 1 million websites.
+	 *
+	 * @var string
+	 */
+	protected string $URL = 'https://downloads.majestic.com/majestic_million.csv';
+
+	/**
+	 * The name of the CSV file containing the top 1 million websites.
+	 *
+	 * @var string
+	 */
+	protected string $CSV_file = 'majestic_million.csv';
+
+	/**
+	 * The number of websites tested so far.
+	 *
+	 * @var int
+	 */
+	protected int $number_of_websites_tested = 0;
+
+	/**
+	 * The number of websites using WordPress.
+	 *
+	 * @var int
+	 */
+	protected int $number_of_websites_using_wordpress = 0;
+
+	/**
+	 * The user agent to use for the requests.
+	 *
+	 * @var string
+	 */
+	protected string $userAgent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36';
+
+	/**
+	 * The number of URLs to send in each batch.
+	 *
+	 * @var int
+	 */
+	protected int $number_of_url_per_chunk = 200;
+
+	/**
+	 * The number of concurrent requests to send.
+	 *
+	 * @var int
+	 */
+	protected int $number_of_concurrent_requests = 200;
+
+	/**
+	 * Show temporary results every X websites tested.
+	 *
+	 * @var int
+	 */
+	protected int $show_temp_results_every = 200;
+
+	/**
+	 * The timeout for each request.
+	 *
+	 * @var int
+	 */
+	protected int $request_timeout = 10;
+
+	/**
+	 * The start time of the script.
+	 *
+	 * @var Carbon
+	 */
+	protected Carbon $start_time;
+
+	/**
+	 * The number of domains to skip from the top of the list.
+	 *
+	 * @var int
+	 */
+	protected int $domain_offset = 0;
 
 	/**
 	 * Create a new command instance.
@@ -41,35 +112,37 @@ class MajesticCommand extends Command {
 	 * Execute the console command.
 	 */
 	public function handle() {
-
+		$this->start_time = Carbon::now();
 		$this->info( 'Scraping Majestic top 1 million websites...' );
 		if ( ( getenv( 'APP_DEBUG' ) == 'false' ) || ( ! Storage::disk( 'local' )->exists( $this->CSV_file ) ) ) {
 			Storage::disk( 'local' )->put( $this->CSV_file, file_get_contents( $this->URL ) );
 		}
-		$path   = Storage::path( $this->CSV_file );
-		$handle = fopen( $path, "r" );
+		$path    = Storage::path( $this->CSV_file );
+		$handle  = fopen( $path, "r" );
+		$URIs    = [];
+		$counter = 0;
 		if ( $handle ) {
 			while ( ( $line = fgetcsv( $handle ) ) !== false ) {
 				try {
-					if ( ! is_numeric( $line[0] ) ) {  // Skip the first line: header
-						continue;
-					}
-					if ( $line[0] < 110000 ) {  // Skip the first 110k websites
-						continue;
-					}
-					$response = Http::withUserAgent( $this->userAgent )->timeout( 7 )->get( $line[6] );
-					if ( $this->is_WordPress( $line[6], $response ) ) {
-						$this->number_of_websites_using_wordpress ++;
-						$this->info( '----> ' . $line[0] . ': ' . $line[6] . ' is using WordPress!' );
+					if ( $counter < $this->number_of_url_per_chunk ) {
+						if ( ! is_numeric( $line[0] ) ) {  // Skip the first line: header
+							continue;
+						}
+						if ( $line[0] < $this->domain_offset ) {  // Skip the first websites from the top of the list.
+							continue;
+						}
+						$URIs[] = $line[6];
+						$counter ++;
 					} else {
-						$this->info( $line[0] . ': ' . $line[6] . ' is not using WordPress!' );
+						$responses = $this->makeConcurrentRequests( $URIs );
+						$this->increase_counters( $responses );
+						$URIs    = [];
+						$counter = 0;
 					}
+
 				} catch ( \Exception $e ) {
-					$this->info( $line[0] . ': ' . $line[6] . ': Error getting this site' );
 					continue;
 				}
-				$this->number_of_websites_tested ++;
-				$this->show_temp_results();
 			}
 		}
 		fclose( $handle );
@@ -77,30 +150,118 @@ class MajesticCommand extends Command {
 		$this->info( 'Scraping finished!' );
 	}
 
-	private function is_WordPress( string $domain, PromiseInterface|\Illuminate\Http\Client\Response $response ): bool {
-		if ( str_contains( $response->header( 'link' ), 'rel="https://api.w.org/"' ) ) {
+	/**
+	 * Check if the domain is using WordPress.
+	 *
+	 * @param string $domain
+	 * @param Response $response
+	 *
+	 * @return bool
+	 */
+	protected function is_WordPress( string $domain, Response $response ): bool {
+		if ( str_contains( $response->getHeaderLine( 'link' ), 'rel="https://api.w.org/"' ) ) {
 			return true;
 		}
-		if ( str_contains( $response->body(), '<meta name="generator" content="WordPress' ) ) {
+		if ( str_contains( $response->getBody(), '<meta name="generator" content="WordPress' ) ) {
 			return true;
 		}
-		if ( str_contains( $response->body(), $domain . '/wp-content/' ) ) {
+		if ( str_contains( $response->getBody(), $domain . '/wp-content/' ) ) {
 			return true;
 		}
-		if ( str_contains( $response->body(), $domain . '/wp-includes/' ) ) {
+		if ( str_contains( $response->getBody(), $domain . '/wp-includes/' ) ) {
 			return true;
 		}
-		if ( str_contains( $response->body(), $domain . '/wp-admin/' ) ) {
+		if ( str_contains( $response->getBody(), $domain . '/wp-admin/' ) ) {
 			return true;
 		}
 
 		return false;
 	}
 
-	private function show_temp_results() {
-		if ( $this->number_of_websites_tested % 50 == 0 ) {
-			$percentage = round( ( $this->number_of_websites_using_wordpress / $this->number_of_websites_tested ) * 100, 2 );
-			$this->info( $this->number_of_websites_tested . ' websites tested so far, ' . $this->number_of_websites_using_wordpress . ' are using WordPress: ' . $percentage . '%' );
+	/**
+	 * Increase the internal counters.
+	 *
+	 * @param array $responses
+	 *
+	 * @return void
+	 */
+	protected function increase_counters( array $responses ): void {
+		foreach ( $responses as $domain => $response ) {
+			if ( $this->is_WordPress( $domain, $response ) ) {
+				$this->number_of_websites_using_wordpress ++;
+			}
+			$this->number_of_websites_tested ++;
+			$this->show_temp_results();
+		}
+	}
+
+	/**
+	 * Show temporary results every X websites tested.
+	 *
+	 * @return void
+	 */
+	protected function show_temp_results(): void {
+		if ( $this->number_of_websites_tested % $this->show_temp_results_every == 0 ) {
+			$percentage          = round( ( $this->number_of_websites_using_wordpress / $this->number_of_websites_tested ) * 100, 2 );
+			$seconds_elapsed     = Carbon::now()->diffInSeconds( $this->start_time );
+			$seconds_per_request = round( $seconds_elapsed / $this->number_of_websites_tested, 3 );
+			$string_to_show      = $this->number_of_websites_tested . ' websites tested so far, ';
+			$string_to_show      .= $this->number_of_websites_using_wordpress . ' are using WordPress: ' . $percentage . '%. ';
+			$string_to_show      .= 'Started ' . $this->start_time->diffForHumans() . ': ' . $seconds_per_request . ' s. per request.';
+			$this->info( $string_to_show );
+		}
+	}
+
+	/**
+	 * Make concurrent requests.
+	 *
+	 * @param array $URIs
+	 *
+	 * @return array
+	 */
+	protected function makeConcurrentRequests( array $URIs ): array {
+		$client            = new Client();
+		$correct_responses = [];
+		try {
+			$requests = function () use ( $URIs, $client ) {
+				foreach ( $URIs as $uri ) {
+					yield function () use ( $client, $uri ) {
+						return $client->getAsync(
+							'https://' . $uri,
+							[
+								'headers'         => [
+									'User-Agent' => $this->userAgent,
+								],
+								'allow_redirects' => true,
+								'timeout'         => $this->request_timeout,
+							] );
+					};
+				}
+			};
+
+			Pool::batch(
+				$client,
+				$requests(),
+				[
+					'concurrency' => $this->number_of_concurrent_requests,
+					'options'     => [
+						'timeout'         => $this->request_timeout,
+						'allow_redirects' => true,
+					],
+					'fulfilled'   => function ( $response, $index ) use ( $URIs, &$correct_responses ) {
+						$correct_responses[ $URIs[ $index ] ] = $response;
+					},
+					'rejected'    => function ( $reason, $index ) use ( $URIs ) {
+						// Don't do anything with failed requests.
+					},
+				]
+			);
+
+
+		} catch ( \Exception $e ) {
+			$this->error( 'Error: ' . $e->getMessage() );
+		} finally {
+			return $correct_responses;
 		}
 	}
 }
